89b40dcade47b7e0e27b8ae42a905294fd1ac68a
MaheshKotambe <technomk99@gmail.com>
2024-01-26 15:50:37 +0530

    Second Commit

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-let moviesList = ['Vikram vedha', 'Faraaz', 'Govinda nam mera', 'Cirkus', 'Nazar Andaj'];
+let moviesList = ['Tanhaji : The Unsung Worrior', 'Sardar Udham', 'Bharat : The Golden Bird', 'The Story Of Mother Cow', 'The Kashmir Files', 'The Kerala Story'];
 let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
 let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
 exports.moviesList = moviesList;
